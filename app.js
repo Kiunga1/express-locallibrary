b3ac1dd77d2c186fc2b15befe9b7b6803a161b43
ann5306 <annkiunga@gmail.com>
2024-06-03 14:58:03 +0300

    Add completed APPLICATION TO GITHUB

--- app.js
+++ app.js
@@ -6,8 +6,35 @@ const logger = require('morgan');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
+const catalogRouter = require("./routes/catalog");
+const compression = require("compression");
+const helmet = require("helmet");
 
+//Create the Express application object
 const app = express();
+
+// Set up rate limiter: maximum of twenty requests per minute
+const RateLimit = require("express-rate-limit");
+const limiter = RateLimit({
+  windowMs: 1 * 60 * 1000, // 1 minute
+  max: 20,
+});
+
+// Apply rate limiter to all requests
+app.use(limiter);
+
+//set up mongoose connection
+const mongoose = require("mongoose");
+mongoose.set("strictQuery", false);
+
+const dev_db_url = 
+
+"mongodb+srv://amukami15:Zha4RJk5TGsonRVj@cluster0.xrfkzyd.mongodb.net/local_library?retryWrites=true&w=majority&appName=Cluster0";
+const mongoDB = process.env.MONGODB_URL || dev_db_url;
+main().catch((err) => console.log(err));
+async function main() {
+  await mongoose.connect(mongoDB)
+}
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -19,8 +46,21 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(compression()); // Compress all routes
+// Add helmet to the middleware chain.
+// Set CSP headers to allow our Bootstrap and Jquery to be served
+app.use(
+  helmet.contentSecurityPolicy({
+    directives: {
+      "script-src": ["'self'", "code.jquery.com", "cdn.jsdelivr.net"],
+    },
+  }),
+);
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+//add catalog routes to middleware chain.
+app.use('/catalog', catalogRouter)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
